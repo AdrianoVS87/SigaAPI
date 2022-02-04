12e574dec7ed6e05e0d1559eda28e6950b36f6ff
Adriano Viera dos Santos <adrianovs.info@gmail.com>
2022-02-04 10:40:15 -0300

    primeiro commit

---
+++ src/test/java/com/healthchess/sigaapi/SigaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthchess.sigaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
